622dbaa09ae6c9729c74f47ea0fa35c3224d3202
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-06-04 19:05:32 -0300

    pushing my solution to 003-reverseint

--- src/challenges/003-reverseint/index.js
+++ src/challenges/003-reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversedNumber = n.toString().split('').reverse().join('')
 
-module.exports = reverseInt;
+  const sign = Math.sign(n)
+
+  return parseInt(reversedNumber) * sign
+}
+
+module.exports = reverseInt
